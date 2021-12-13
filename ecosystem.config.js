42d1043d19e14bef39eddeb0c0ba33894f2d638d
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2021-12-13 17:36:05 +0100

    Trying to make Heroku work

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,15 @@ module.exports = {
     instances: "max",
     exec_mode: 'cluster',
     watch: '.',
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
+    // env: {
+    //   NODE_ENV: "development",
+    // },
+    // // env_production: {
+    // //   NODE_ENV: "production"
+    // // },
+    // bazinga: {
+    //   NODE_ENV: "production",
+    //   // PORT: (process.env.PORT) ? process.env.PORT : 3333
+    // }
   }]
 };
