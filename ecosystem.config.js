53775da9313bb4aa47684b4f0ed80d8b70d920c8
Nico Arqueros <nico@dcspark.io>
2021-12-12 22:02:00 -0600

    update to cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'allowlist',
     script: './build/index.js',
     instances: "max",
+    exec_mode: 'cluster',
     watch: '.',
     env: {
       NODE_ENV: "development",
