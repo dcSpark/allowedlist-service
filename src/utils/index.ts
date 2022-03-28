ae27bf886881bef51775973e1ed4a1910ef31895
bartfish <bartlomiejrybaa@gmail.com>
2022-03-28 16:52:42 +0200

    new allowed addresses & filtering out function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -65,3 +65,26 @@ export const loadAddressesFromCSV = (): Promise<Set<string>> => {
             });
     });
 };
+
+/// Helper function, to filter out all non-evm addresses
+export const filterAddresses = (): Promise<string[]> => {
+    return new Promise((resolve, _reject) => {
+        const dataFromCSV: string[] = [];
+        fs.createReadStream(path.resolve(__dirname, "./files/r.csv")) // files/name_of_file_to_filter.csv
+            .pipe(parse({ delimiter: "," }))
+            .on("data", (csvrow: Array<string>) => {
+                if (isAddress(csvrow[0])) {
+                    dataFromCSV.push(csvrow[0]);
+                }
+            })
+            .on("end", () => {
+                const file = fs.createWriteStream(path.resolve(__dirname, "./files/r_new.txt"), "utf-8");
+                dataFromCSV.forEach((v: string) => {
+                    // console.log(v);
+                    file.write(v + ",\n", "utf-8");
+                });
+                file.end();
+                resolve(dataFromCSV);
+            });
+    });
+};
