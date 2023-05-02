59baf8a4902eefa427dad20f896cba11152a5953
bartfish <bartlomiejrybaa@gmail.com>
2023-05-02 13:52:18 +0200

    shared: extend shared type

--- shared/types.ts
+++ shared/types.ts
@@ -1,17 +1,17 @@
 export type MilkomedaStargateAsset = {
-    idCardano: string; // 0-right-padded fingerprint
-    idMilkomeda: string; // ERC20 contract address
-    minCNTInt?: string; // min amount of Cardano Native Token
-    minGWei?: string; // optional - whatever GWei conversion is
+  idCardano: string; // 0-right-padded fingerprint
+  idMilkomeda: string; // ERC20 contract address
+  minCNTInt?: string; // min amount of Cardano Native Token
+  minGWei?: string; // optional - whatever GWei conversion is
+};
+export type MilkomedaStargateResponse = {
+  current_address: string;
+  sidechain_address: string;
+  ttl_expiry: number;
+  ada: {
+    minLovelace: string;
+    fromADAFeeLovelace: string;
+    toADAFeeGWei: string;
   };
-  export type MilkomedaStargateResponse = {
-    current_address: string;
-    ttl_expiry: number;
-    ada: {
-      minLovelace: string;
-      fromADAFeeLovelace: string;
-      toADAFeeGWei: string;
-    };
-    assets: Array<MilkomedaStargateAsset>;
-  };
-  
+  assets: Array<MilkomedaStargateAsset>;
+};
